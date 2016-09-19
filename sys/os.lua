b937305c5992c9eeab01cdd4b33e210f13c23970
Unknown <piorjadelp@gmail.com>
2016-09-19 21:51:43 +0200

    MAJOR UPDATE
    - Made it impossible for programs to edit the FS API and/or things like _G.currentUsr to grant you root access :) (thx to Sewbacca)
    - Added sudo command (asks for root password to give a program temporarely root access)

--- sys/os.lua
+++ sys/os.lua
@@ -1,3 +1,46 @@
+
+local tTables = {}
+local tReadOnly = {}
+local native_rawset = rawset
+local mt = {
+                __metatable = 'Attempt to get protected metatable',
+                __newindex = function(self, key, value)
+                        if not tTables[self][key] then
+                                native_rawset(self, key, value)
+                                return
+                        end
+                        error('Try to write to read only table', 2)
+                end
+}
+mt.__index = function (self, key)
+                local var = tTables[self][key]
+                if type(var) == 'table' then
+                                setmetatable({}, mt)
+                end
+                return var
+end
+
+_G.rawset = function (tab, key, value)
+                if tReadOnly[tab] then error('Try to write to read only table', 2) end
+                return native_rawset(tab, key, value)
+end
+
+local function createReadOnly (tab)
+                if type(tab) ~= 'table' then
+                                error('table expected, got ' .. type(tab), 2)
+                end
+                local self = setmetatable({}, mt)
+                tTables[self] = tab
+                tReadOnly[self] = true
+                return self
+end
+
+
+
+
+
+
+local function init()
 --[[
 	CLinux
 	A Linux-tryhard OS
@@ -15,7 +58,6 @@
 ]]
 --APIs (WILL PROBABLY BE IMPORTANT FOR DESKTOP ENVIROMENTS TOO)
 os.loadAPI("/sys/API/sha")
-_G.sha = sha
 X = nil --ENTER THE PATH TO YOUR DESKTOP ENVIROMENT, IF IT'S NOT NIL IT WILL TRY TO LOAD IT
 
 --Variablen
@@ -25,16 +67,84 @@ os.version = function()
 end
 
 _verstr = os.version()
-usrData = {
+local usrData = {
 	usrName = {},
 	password = {},
 }
-tmpUsr = ""
-tmpPw = ""
-currentDir = "/usr/"
-currentUsr = ""
-cUsrNmbr = 1
+local tmpUsr = ""
+local tmpPw = ""
+local cUsrNmbr = 1
+local oldCUsr = ""
+local oldCPw = ""
+local oldCDir = ""
+_G.currentUsr = ""
+_G.currentPw = ""
+_G.currentDir = ""
 
+_G.currentDir = "/usr/"
+
+local function changeDir(dest)
+	local goback = false
+		--local a, b = string.find(dest, "[..]")
+		if dest == ".." then
+			goback = true
+		else
+			goback = false
+		end
+	if dest == "~" then
+		dest = "/usr/".._G.currentUsr.."/home/"
+	end
+	local a, b = string.find(dest, "//")
+	local falseDir = false
+	if a ~= nil and a >= 1 and b > 1 and goback == false then
+		falseDir = true
+	end
+	local a, b = string.find(dest, "/", 1)
+	if a == 1 and goback == false and falseDir == false then
+		--inthis = false
+	elseif falseDir == false then
+		--inthis = true
+		dest = _G.currentDir..dest
+	end
+	if fs.isDir(dest) and falseDir == false then
+		local found = false
+		
+		
+		
+		local a, b = string.find(dest, "/", #dest)
+		if a == #dest and goback == false then
+			found = true
+		end
+		if found == false and goback == false then
+			_G.currentDir = dest.."/"
+		elseif found == true and goback == false then
+			_G.currentDir = dest
+		elseif goback and _G.currentDir ~= "/" then
+			dest = _G.currentDir
+			dest = string.reverse(dest)
+			local i, j = string.find(dest, "/")
+			if i then
+				dest = string.sub(dest, j+1, #dest)
+				local i, j = string.find(dest, "/")
+				dest = string.sub(dest, j+1, #dest)
+				dest = string.reverse(dest)
+				_G.currentDir = dest.."/"
+			else
+				local col = term.getTextColor()
+				term.setTextColor(colors.red)
+				print("Error.")
+				term.setTextColor(col)
+			end
+		end
+		return true
+	else
+		local col = term.getTextColor()
+		term.setTextColor(colors.red)
+		print("No such directory.")
+		term.setTextColor(col)
+		return false
+	end
+end
 --Backup
 
 local oldOpen = fs.open
@@ -42,7 +152,36 @@ local oldmkdir = fs.makeDir
 local oldMove = fs.move
 local oldCopy = fs.copy
 local oldDelete = fs.delete
+
+--Wichtige tabellen/variablen für den coroutine-manager
+
+local c = {
+	c = {},--Here are the saved coroutines in a table, basically I will add the command "ps", to list the processes using the PIDs (basically the keys in every entry, e.g. 1,2,3,4,etc.)
+	w = {}, --The individual window of every coroutines, mainly used to prevent redrawing the screen all the time
+} 
+
 --Funktionen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function clear(bg, fg) --did you know that you can see this function in any of my codes? xD
 	term.setCursorPos(1,1)
@@ -50,8 +189,6 @@ function clear(bg, fg) --did you know that you can see this function in any of m
 	term.setTextColor(fg)
 	term.clear()
 end
-
-_G.clear = clear
 
 function limitRead(nmbr, a)
 	term.setCursorBlink(true)
@@ -86,7 +223,6 @@ function limitRead(nmbr, a)
 	end
 end
 
-_G.limitRead = limitRead
 
 function limitReadPw(nmbr)
 	term.setCursorBlink(true)
@@ -113,7 +249,6 @@ function limitReadPw(nmbr)
 	end
 end
 
-_G.limitReadPw = limitReadPw
 
 local function register(step)
 	clear(colors.black, colors.white)
@@ -196,11 +331,11 @@ local function login(step)
 		else
 			for _, name in ipairs(usrData.usrName) do
 				if name == e then
-					currentUsr = e
+					_G.currentUsr = e
 					cUsrNmbr = _
 					login(2)
 				elseif e == "root" then
-					currentUsr = e
+					_G.currentUsr = e
 					cUsrNmbr = 0
 					login(2)
 				elseif _ == #usrData.usrName then
@@ -223,18 +358,18 @@ local function login(step)
 			term.setTextColor(col)
 			login(2)
 		else
-			p = sha.pbkdf2(p, currentUsr, 10):toHex()
+			p = sha.pbkdf2(p, _G.currentUsr, 10):toHex()
 			p = tostring(p)
 			local file = fs.open("/sys/.rootpw","r")
 			local rpw = file.readLine()
 			file.close()
-			if currentUsr ~= "root" and p ~= usrData.password[cUsrNmbr] then
+			if _G.currentUsr ~= "root" and p ~= usrData.password[cUsrNmbr] then
 				local col = term.getTextColor()
 				term.setTextColor(colors.red)
 				print("Oops, that didn't work! Let's try it again.")
 				term.setTextColor(col)
 				login(1)
-			elseif currentUsr == "root" and p ~= rpw then
+			elseif _G.currentUsr == "root" and p ~= rpw then
 				local col = term.getTextColor()
 				term.setTextColor(colors.red)
 				print("Oops, that didn't work! Let's try it again.")
@@ -242,11 +377,12 @@ local function login(step)
 				login(1)
 			else
 				--stuff
-				currentDir = "/usr/"..currentUsr.."/home/"
-				_G.vDir = "/usr/"..currentUsr.."/home/"
-				_G.currentUsr = currentUsr
-				_G.currentDir = currentDir
+				_G.currentDir = "/usr/".._G.currentUsr.."/home/"
+				--_G.vDir = "/usr/".._G.currentUsr.."/home/"
+				_G.currentUsr = _G.currentUsr
+				_G.currentDir = _G.currentDir
 				_G.currentPw = p
+				--_G.currentPw = p
 				print("Success.")
 				--limitFunctions()
 				linuxShell()
@@ -258,7 +394,7 @@ end
 
 
 function limitFunctions()
-	
+
 	fs.open = function(path, mode)
 		if mode ~= "a" or mode ~= "w" or mode ~= "r" or mode ~= "br" or mode ~= "bw" then
 			return nil
@@ -276,6 +412,7 @@ function limitFunctions()
 			local c = oldOpen("/sys/.rootpw","r")
 			local rpw = c.readLine()
 			c.close()
+			--if _G.currentPw == rpw then
 			if _G.currentPw == rpw then
 				return oldOpen(path, mode)
 			else
@@ -301,8 +438,10 @@ function limitFunctions()
 			local c = oldOpen("/sys/.rootpw","r")
 			local rpw = c.readLine()
 			c.close()
+			--if _G.currentPw == rpw and fs.exists(path) == false then
 			if _G.currentPw == rpw and fs.exists(path) == false then
 				return oldmkdir(path)
+			--elseif _G.currentPw ~= rpw then
 			elseif _G.currentPw ~= rpw then
 				return false
 			elseif fs.exists(path) then
@@ -337,6 +476,7 @@ function limitFunctions()
 			local c = oldOpen("/sys/.rootpw","r")
 			local rpw = c.readLine()
 			c.close()
+			--if _G.currentPw == rpw and fs.exists(oldPath) then
 			if _G.currentPw == rpw and fs.exists(oldPath) then
 				return oldMove(oldPath, newPath)
 			elseif fs.exists(oldPath) == false then
@@ -368,6 +508,7 @@ function limitFunctions()
 			local c = oldOpen("/sys/.rootpw","r")
 			local rpw = c.readLine()
 			c.close()
+			--if _G.currentPw == rpw and fs.exists(oldPath) and fs.exists(newPath) == false then
 			if _G.currentPw == rpw and fs.exists(oldPath) and fs.exists(newPath) == false then
 				return oldOpen(oldPath, newPath)
 			elseif fs.exists(oldPath) == false then
@@ -401,6 +542,7 @@ function limitFunctions()
 			local c = oldOpen("/sys/.rootpw","r")
 			local rpw = c.readLine()
 			c.close()
+			--if _G.currentPw == rpw and fs.exists(path) then
 			if _G.currentPw == rpw and fs.exists(path) then
 				return oldDelete(path)
 			elseif fs.exists(path) == false then
@@ -416,6 +558,17 @@ function limitFunctions()
 			end
 		end
 	end
+	oldCUsr = _G.currentUsr
+	oldCPw = _G.currentPw
+	oldCDir = _G.currentDir
+	--[[local sandBox = setmetatable({}, {
+		__index = _G
+	})]]
+
+
+
+	--restoreFunctions()
+	--return sandBox
 end
 
 local function restoreFunctions()
@@ -424,6 +577,9 @@ local function restoreFunctions()
 	fs.delete = oldDelete
 	fs.move = oldMove
 	fs.copy = oldCopy
+	_G.currentUsr = oldCUsr
+	_G.currentPw = oldCPw
+	_G.currentDir = oldCDir
 end
 
 function linuxShell()
@@ -432,18 +588,18 @@ function linuxShell()
 	local d = "/"
 
 	while loop do
-		currentDir = _G.currentDir
+		_G.currentDir = _G.currentDir
 		local x, y = term.getCursorPos()
 		term.setCursorPos(1,y)
 		term.setTextColor(colors.yellow)
-		local a, b = string.find(currentDir, "/usr/"..currentUsr.."/home/")
+		local a, b = string.find(_G.currentDir, "/usr/".._G.currentUsr.."/home/")
 		if a then
-			d = string.gsub(currentDir, "/usr/"..currentUsr.."/home/", "~/", 1)
+			d = string.gsub(_G.currentDir, "/usr/".._G.currentUsr.."/home/", "~/", 1)
 		else
-			d = currentDir
+			d = _G.currentDir
 		end
 		
-		term.write(currentUsr.."@ ")
+		term.write(_G.currentUsr.."@ ")
 		term.setTextColor(colors.blue)
 		term.write(d.."> ")
 		term.setTextColor(colors.white)
@@ -451,10 +607,6 @@ function linuxShell()
 		local command = read()
 		local args = {}
 		local arg = ""
-		--[[local function insert(str)
-			table.insert(cTbl, str)
-		end]]
-		--string.gsub(command, "(%w+)", insert)
 		local i, j = string.find(command, " ")
 		if i == nil then
 			command = command
@@ -471,8 +623,8 @@ function linuxShell()
 				local i, j = string.find(arg, " ")
 				if i ~= nil then
 					local a = string.sub(arg, 1, i-1)
-					local i, j = string.find(a, "~/")
-					if i == 1 and j == 2 then
+					local x, y = string.find(a, "~/")
+					if x == 1 and y == 2 then
 						local c = string.sub(a, 3, #a)
 						a = "/usr/".._G.currentUsr.."/home/"..c
 					end
@@ -488,21 +640,161 @@ function linuxShell()
 				end
 			until i == nil
 		end
-		if fs.exists("/usr/bin/"..command) then
-			--stuff
-			local a = loadfile("/usr/bin/"..command)
-			limitFunctions()
-			--a(unpack(args))
-			local ok, err = pcall(a, unpack(args))
-			if ok == false then
-				local col = term.getTextColor()
-				term.setTextColor(colors.red)
-				print(err)
-				term.setTextColor(col)
+		local sudo = false
+		if command == "sudo" then
+			if #args > 0 then
+				sudo = true
+				command = args[1]
+				table.remove(args, 1)
+			else
+				sudo = false
+				command = " "
 			end
-			restoreFunctions()
-			--os.run({}, "/usr/bin/"..command, unpack(args))
+		end
 
+		if fs.exists("/usr/bin/"..command) and command ~= "cd" then
+			if sudo == false then
+				fs.copy("/usr/bin/"..command, "/tmp/running")
+				local file = fs.open("/tmp/running", "r")
+				local inhalt = file.readAll()
+				file.close()
+				local file = fs.open("/tmp/running", "w")
+				file.write("local function init()\n")
+				file.write(inhalt.."\n")
+				file.write("end\n")
+				file.write("init()")
+				file.close()
+				local a = loadfile("/usr/bin/"..command)
+
+				limitFunctions()
+				local e = getfenv(init)
+				local sandBox = createReadOnly(e)
+				setfenv(a, sandBox)
+				local ok, err = pcall(a, unpack(args))
+				restoreFunctions()
+				if ok == false or ok == nil then
+					local col = term.getTextColor()
+					term.setTextColor(colors.red)
+					print(err)
+					term.setTextColor(col)
+				end
+				fs.delete("/tmp/running")
+			elseif sudo == true then
+				sudo = false
+				term.write("Please enter root password: ")
+				local p = limitReadPw(99)
+				print("")
+				local file = fs.open("/sys/.rootpw", "r")
+				local rpw = file.readLine()
+				file.close()
+				p = sha.pbkdf2(p, "root", 10):toHex()
+				p = tostring(p)
+				if #p < 1 or p ~= rpw then
+					local c = term.getTextColor()
+					term.setTextColor(colors.red)
+					print("Wrong password.")
+					term.setTextColor(c)
+				elseif p == rpw then
+					local a = loadfile("/usr/bin/"..command)
+					limitFunctions()
+					local oldCUsr = _G.currentUsr
+					local oldCPw = _G.currentPw
+					_G.currentUsr = "root"
+					_G.currentPw = rpw
+					local e = getfenv(init)
+					local sandBox = createReadOnly(e)
+					setfenv(a, sandBox)
+					local ok, err = pcall(a, unpack(args))
+					restoreFunctions()
+					if ok == false or ok == nil then
+						local col = term.getTextColor()
+						term.setTextColor(colors.red)
+						print(err)
+						term.setTextColor(col)
+					end
+				end
+			end
+--[[							NOT YET IMPLEMENTED:
+
+
+
+
+		elseif fs.exists("/usr/bin/"..command) and command ~= "cd" then
+			if sudo == false then
+				local nProc = #c.c+1
+				fs.copy("/usr/bin/"..command, "/tmp/"..tostring(nProc))
+				local file = fs.open("/tmp/"..tostring(nProc), "r")
+				local inhalt = file.readAll()
+				file.close()
+				local file = fs.open("/tmp/"..tostring(nProc), "w")
+				file.write("local function init()\n")
+				file.write(inhalt.."\n")
+				file.write("end\n")
+				file.write("init()")
+				file.close()
+
+
+
+				local a = loadfile("/usr/bin/"..command)
+
+				limitFunctions()
+				local e = getfenv(init)
+				local sandBox = createReadOnly(e)
+				setfenv(a, sandBox)
+				--a, unpack(args)
+				c.c[nProc] = coroutine.create(pcall)
+				c.w[nProc] = window.create(oldTerm, 1, 1, 51, 19)
+				term.redirect(c.w[nProc])
+				clear(colors.black, colors.white)
+				inProgram = nProc
+				restoreFunctions()
+			elseif sudo == true then
+				sudo = false
+				term.write("Please enter root password: ")
+				local p = limitReadPw(99)
+				local file = fs.open("/sys/.rootpw", "r")
+				local rpw = file.readLine()
+				file.close()
+				p = sha.pbkdf2(p, "root", 10):toHex()
+				p = tostring(p)
+				if #p < 1 or p ~= rpw then
+					local c = term.getTextColor()
+					term.setTextColor(colors.red)
+					print("Wrong password.")
+					term.setTextColor(c)
+				elseif p == rpw then
+					local nProc = #c.c+1
+					local a = loadfile("/usr/bin/"..command)
+					limitFunctions()
+					local oldCUsr = _G.currentUsr
+					local oldCPw = _G.currentPw
+					_G.currentUsr = "root"
+					_G.currentPw = rpw
+					local e = getfenv(init)
+					local sandBox = createReadOnly(e)
+					setfenv(a, sandBox)
+					c.c[nProc] = coroutine.create(pcall)
+					c.w[nProc] = window.create(oldTerm, 1, 1, 51, 19)
+					term.redirect(c.w[nProc])
+					clear(colors.black, colors.white)
+					inProgram = nProc
+					restoreFunctions()
+				end
+			end
+]]
+
+
+
+
+
+
+		elseif command == "cd" then
+			if #args < 1 or #args > 1 then
+				print("Usage:")
+				print("		cd <path>")
+			else
+				changeDir(args[1])
+			end
 		elseif fs.exists("/usr/bin/"..command) == false then
 			local col = term.getTextColor()
 			term.setTextColor(colors.red)
@@ -513,7 +805,6 @@ function linuxShell()
 	end
 end
 
-_G.linuxShell = linuxShell
 
 local function checkUsr()
 	print("Welcome to ".._verstr.."!")
@@ -552,4 +843,9 @@ end
 --Code
 
 clear(colors.black, colors.white)
+fs.delete("/tmp/*")
 checkUsr()
+
+end
+
+init()
